8c0dc41bd3c535c7fec045361f54a36d930f4d7d
Vidhi Ratan <vidhi.ratan.btech2024@sitpune.edu.in>
2025-08-12 21:54:30 +0530

    Create Declare and Initialize a Strings.cpp

---
+++ Declare and Initialize a Strings.cpp
@@ -0,0 +1,30 @@
+// Vidhi Ratan
+// 24070123131
+// B2
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+    string str1 = "Hello, World!";
+    cout << "String 1: " << str1 << endl;
+
+    string str2;
+    str2 = "Hello from C++!";
+    cout << "String 2: " << str2 << endl;
+
+    char str3[] = "Hello, Array!";
+    cout << "String 3: " << str3 << endl;
+
+    return 0;
+}
+
+/*
+Output:
+String 1: Hello, World!
+String 2: Hello from C++!
+String 3: Hello, Array!
+*/
